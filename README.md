0ff97c15c36feaebb1fb7a25a79e0ea7385d8b4f
outragedline <diegojanson31@outlook.com>
2022-08-27 22:29:31 -0300

    fixed .gitignore, added automatic instalation file

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-This is my neovim config, you can use it if you want
+This is my neovin config, if you want to use, you can install with that command in archlinux
 
-I use it in archlinux, you can install all the dependencies with the install.sh
+bash <(curl -s https://raw.githubusercontent.com/outragedline/nvim/main/install.sh)
 
-if you are in another distro, you can see the dependencies and install manually
-# nvim
+In another distro you can see the dependecies and install manually
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo pacman -Syu neovim nodejs-lts-gallium python3 python-pip ripgrep fd lua fzf gcc  npm tree-sitter lua-language-server git
+
+pip install wheel pynvim autopep8 flake8
+
+git clone https://github.com/outragedline/nvim ~/.config/nvim
+
+git clone https://github.com/microsoft/java-debug ~/.config/nvim/java-debug
+cd ~/.config/nvim/java-debug
+./mvnw clean install
+
+git clone https://github.com/microsoft/vscode-java-test.git ~/.config/nvim/vscode-java-test
+cd ~/.config/nvim/vscode-java-test
+npm install
+npm run build-plugin
