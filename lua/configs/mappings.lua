afb890008f30c38a88c0bb43290fde3b6ae9999b
outragedline <diegojanson31@outlook.com>
2022-10-18 00:24:26 -0300

    updated cmp to fix deprecated things

--- lua/configs/mappings.lua
+++ lua/configs/mappings.lua
@@ -19,13 +19,9 @@ keymap("n", "<C-A-q>", "<cmd> q<CR>", fopts) -- close current file
 keymap("n", "<C-u>", "<cmd> u <CR>", opts) --undo
 keymap("n", "<C-r>", "<cmd> redo <CR>", opts) --redo
 
-
-
 --create splits
 keymap("n", "<C-h>", "<cmd> split<CR>", opts) --hotizontal split
 keymap("n", "<C-j>", "<cmd> vsplit<CR>", opts) -- vertical split
-
-
 
 -- Resize splits with arrows
 keymap("n", "<C-Up>", "<cmd> resize +2<CR>", opts)
@@ -38,29 +34,22 @@ keymap("n", "<C-A-Down>", "<C-w>j", opts)
 keymap("n", "<C-A-Up>", "<C-w>k", opts)
 keymap("n", "<C-A-Right>", "<C-w>l", opts)
 
-
-
 -- visual mappings
 keymap("v", "<", "<gv", opts) -- unindent lines
 keymap("v", ">", ">gv", opts) -- indent lines
 
-keymap("v", "p", '_dP', fopts) --"paste"  in visual mode replace selected text with the yanked text
-
+keymap("v", "p", "_dP", fopts) --"paste"  in visual mode replace selected text with the yanked text
 
 -- terminal mappings
-keymap('t', '<esc>', [[<C-\><C-n>]], opts)
-keymap('t', '<C-x>', [[<C-\><C-n>]], opts)
-
-
+keymap("t", "<esc>", [[<C-\><C-n>]], opts)
+keymap("t", "<C-x>", [[<C-\><C-n>]], opts)
 
 --tools
-keymap('n', "<C-a>", "<cmd> NvimTreeToggle<CR>", opts)
-keymap('n', "<C-o>", "<cmd> SymbolsOutline<CR>", opts)
-keymap('n', '<leader>ch', '<cmd>noh<cr>', opts)
+keymap("n", "<C-a>", "<cmd> NvimTreeToggle<CR>", opts)
+keymap("n", "<C-o>", "<cmd> SymbolsOutline<CR>", opts)
+keymap("n", "<leader>ch", "<cmd>noh<cr>", opts)
 
 keymap("n", "<C-f>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", fopts)
-
-
 
 -- e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
 keymap("n", "<C-n>", "<cmd> BufferLineCycleNext<CR>", opts)
@@ -69,20 +58,17 @@ keymap("n", "<C-p>", "<cmd> BufferLineCyclePrev<CR>", opts)
 -- close current buffer
 keymap("n", "<C-A-d>", "<cmd> bdelete!<CR>", opts)
 
-
 -- move around tabs
-keymap("n", "<C-A-n>", "<cmd> tabnext<CR>", fopts)
-keymap("n", "<C-A-p>", "<cmd> tabprevious<CR>", fopts)
+keymap("n", "<C-A-n>", "<cmd>tabnext<CR>", fopts)
+keymap("n", "<C-A-p>", "<cmd>tabprevious<CR>", fopts)
+keymap("n", "<leader>ts", "<cmd>tab split<CR>", fopts)
 
 -- create new tab
 keymap("n", "<leader>tn", "<cmd> tabnew<CR>", opts)
 keymap("n", "<leader>tc", "<cmd> tabclose<CR>", opts)
 
-
-
 -- close current tab
 keymap("n", "<leader>td", "<cmd> tabclose!<CR>", opts)
-
 
 -- These commands will move the current buffer backwards or forwards in the bufferline
 keymap("n", "<leader>mn", "<cmd> BufferLineMoveNext<CR>", opts)
@@ -92,8 +78,6 @@ keymap("n", "<leqder>mp", "<cmd> BufferLineMovePrev<CR>", opts)
 keymap("n", "<leader>se", "<cmd> BufferLineSortByExtension<CR>", opts)
 keymap("n", "<leader>sd", "<cmd> BufferLineSortByDirectory<CR>", opts)
 
-
-
 --Telescope mappings
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
@@ -102,13 +86,10 @@ keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
 keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", opts)
 keymap("n", "<leader>fp", "<cmd>Telescope projects<cr>", opts)
 
-
-
 --Gitsigns
 keymap("n", "<leader>gs", "<cmd> Gitsigns show<CR>", opts)
 keymap("n", "<leader>gt", "<cmd> Gitsigns toggle_deleted<CR>", opts)
 keymap("n", "<leader>gd", "<cmd> Gitsigns diffthis<CR>", opts)
-
 
 -- DAP
 keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
@@ -118,11 +99,9 @@ keymap("n", "<leader>dru", "<cmd>lua require'dapui'.toggle({reset=true})<cr>", o
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 keymap("n", "<leader>dr", "<cmd> DapToggleRepl<cr>", opts)
 
-
 -- Packer
 keymap("n", "<leader>ps", "<cmd>PackerSync<cr>", opts)
 
 -- Copilot
 keymap("n", "<leader>cp", "<cmd>CopilotPanel<cr>", opts)
 keymap("i", "<C-c>", "<cmd>CopilotPanel<cr>", opts)
-
