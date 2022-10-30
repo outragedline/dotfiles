9f52f25831b6e989dd2ea79bcf578cb170f10dda
outragedline <diegojanson31@outlook.com>
2022-10-30 04:52:25 -0300

    Update: treesitter config -> add some languages to ensure installed

--- lua/ui/treesitter.lua
+++ lua/ui/treesitter.lua
@@ -4,32 +4,29 @@ if not TS_status_ok then
 end
 
 local synxtax_disable_list = {} --add a language  here to disable TreeSitter highlight for this lang
-local indent_disable_list = {"python"} --add a language  here to disable TreeSitter indent for this lang
+local indent_disable_list = { "python" } --add a language  here to disable TreeSitter indent for this lang
 
-
-configs.setup {
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    additional_vim_regex_highlighting = true,
-		disable = {synxtax_disable_list}
-
-  },
-  indent = { enable = true,
-disable = indent_disable_list
-},
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
-  autopairs = {
+configs.setup({
+	ensure_installed = { "lua", "python", "java", "json", "bash", "http", "html" },
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		additional_vim_regex_highlighting = true,
+		disable = { synxtax_disable_list },
+	},
+	indent = { enable = true, disable = indent_disable_list },
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+	autopairs = {
 		enable = true,
 	},
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-}
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+})
