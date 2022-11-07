faff8406ebaa4cbfca40cd60633e2aaf10a33a28
outragedline <diegojanson31@outlook.com>
2022-11-07 07:30:38 -0300

    Enable transparency

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -6,8 +6,12 @@ end
 onedark.setup({
 	-- Main options --
 	style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = true,
 	highlights = {
 		MatchParen = { bg = "#37383d", fg = "none" },
+	},
+	lualine = {
+		transparent = true, -- lualine center bar transparency
 	},
 })
 vim.cmd("colorscheme onedark")
