3d567c406099345b705fec550b0b596f33515780
outragedline <diegojanson31@outlook.com>
2022-11-09 09:00:17 -0300

    Fix notify color error

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -21,11 +21,11 @@ notify.setup {
   render = "default",
 
   -- Default timeout for notifications
-  timeout = 175,
+  timeout = 300,
 
   -- For stages that change opacity this is treated as the highlight behind the window
   -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-  background_colour = "Normal",
+  background_colour = "#000000",
 
   -- Minimum width for notification windows
   minimum_width = 10,
