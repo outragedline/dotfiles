9314cf06566f180767a62161822c99eeafef2856
outragedline <diegojanson31@outlook.com>
2022-11-23 20:43:40 -0300

    Change lualine layout

--- lua/ui/lualine.lua
+++ lua/ui/lualine.lua
@@ -7,8 +7,8 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = { "alpha", "dashboard", "NvimTree", "Outline" },
 		},
@@ -23,7 +23,7 @@ lualine.setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "branch", "diff", "diagnostics", "filename" },
+		lualine_c = { "filename", "filesize" },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
