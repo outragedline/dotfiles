6f3340c54ad7b13f3f2032e515deb653a1322b8d
outragedline <81770118+outragedline@users.noreply.github.com>
2022-11-24 11:19:19 -0300

    Remove useless denpendencies

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu neovim nodejs-lts-gallium python3 python-pip ripgrep fd lua fzf gcc  npm tree-sitter  jdk17-openjdk  google-java-format
+sudo pacman -Syu neovim nodejs-lts-gallium python3 python-pip ripgrep fd lua fzf gcc npm
 
 pip install wheel pynvim 
 
