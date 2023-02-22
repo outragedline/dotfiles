a784cf0dd740630cff1049ae225fb21392aabd41
outragedline <outragedline@proton.me>
2023-02-22 02:52:07 -0300

    do something and dont remember and dont care about

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -8,6 +8,9 @@ onedark.setup({
 	-- Main options --
 	style = "cool", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 	transparent = true,
+	highlights = {
+		MatchParen = { bg = "#37383d", fg = "none" },
+	},
 	lualine = {
 		transparent = false, -- lualine center bar transparency
 	},
