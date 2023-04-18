32d0ac1af7e53e3f3a1a91fd07ebcbb46b37b5f7
outragedline <outragedline@proton.me>
2023-04-18 17:36:20 -0300

    Installation script: first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Hyprland basic packages
+yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty
+
+# Other important applications
+yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger telegram-desktop ungoogled-chromium-bin mpv viewnior htop thunderbird
