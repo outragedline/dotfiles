8f82b08245441eabddc6f9ad6f4943f848c6409f
outragedline <outragedline@proton.me>
2023-04-19 00:37:25 -0300

    Installation script: move neovim installation to general script

--- .config/nvim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Syu neovim nodejs-lts-gallium python3 python-pip ripgrep fd lua fzf gcc npm
-
-pip install wheel pynvim 
-
-git clone https://github.com/outragedline/nvim ~/.config/nvim
--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 # Hyprland basic packages
 yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty
 
+# Neovim
+yay -S neovim nodejs-lts-gallium python3 python-pip ripgrep fd lua fzf gcc npm
+
+pip install wheel pynvim 
+
 # Other important applications
-yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger telegram-desktop ungoogled-chromium-bin mpv viewnior htop thunderbird
+yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger telegram-desktop ungoogled-chromium-bin mpv viewnior htop thunderbird zsh
+
+
