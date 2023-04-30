6cfc3f0bf29aeba7f7b9f6fbf358fb250a2b258c
outragedline <outragedline@proton.me>
2023-04-30 03:33:37 -0300

    Nvim: comment onedark setup to load faster

--- .config/nvim/lua/ui/colorscheme.lua
+++ .config/nvim/lua/ui/colorscheme.lua
@@ -1,19 +1,19 @@
-local onedark_status_ok, onedark = pcall(require, "onedark")
-if not onedark_status_ok then
-	return
-end
-
-onedark.setup({
-	-- Main options --
-	style = "cool", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-	transparent = true,
-	highlights = {
-		MatchParen = { bg = "#37383d", fg = "none" },
-	},
-	lualine = {
-		transparent = false, -- lualine center bar transparency
-	},
-})
+--[[ local onedark_status_ok, onedark = pcall(require, "onedark") ]]
+--[[ if not onedark_status_ok then ]]
+--[[ 	return ]]
+--[[ end ]]
+--[[]]
+--[[ onedark.setup({ ]]
+--[[ 	-- Main options -- ]]
+--[[ 	style = "cool", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light' ]]
+--[[ 	transparent = true, ]]
+--[[ 	highlights = { ]]
+--[[ 		MatchParen = { bg = "#37383d", fg = "none" }, ]]
+--[[ 	}, ]]
+--[[ 	lualine = { ]]
+--[[ 		transparent = false, -- lualine center bar transparency ]]
+--[[ 	}, ]]
+--[[ }) ]]
 
 local catppuccin_status_ok, catppuccin = pcall(require, "catppuccin")
 if not catppuccin_status_ok then
@@ -38,7 +38,7 @@ catppuccin.setup({
 	styles = {
 		comments = { "italic" },
 		conditionals = { "italic" },
-		loops = {'italic'},
+		loops = { "italic" },
 		functions = {},
 		keywords = {},
 		strings = {},
@@ -53,12 +53,11 @@ catppuccin.setup({
 	custom_highlights = {},
 	integrations = {
 		cmp = true,
-		gitsigns = true,
+		gitsigns = false,
 		nvimtree = true,
 		telescope = true,
 		notify = true,
 	},
 })
 
--- setup must be called before loading
 vim.cmd.colorscheme("catppuccin")
