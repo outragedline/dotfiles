43861dc491f6748d706576b72cec2f17a772d6d1
outragedline <outragedline@proton.me>
2023-05-03 19:05:08 -0300

    Nvim & Waybar: Update icons due to nerdfonts breaking changes

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,4 +1,5 @@
-local alpha = require('alpha')
+local icons = require("icons")
+local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
 	[[                               __                ]],
@@ -9,16 +10,14 @@ dashboard.section.header.val = {
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("f", icons.ui.Search .. "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", icons.documents.File .. "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", icons.documents.Folder .. " Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", icons.ui.History .. "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", icons.kind.Text .. "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", icons.ui.Gear .. "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", icons.ui.SignOut .. "  Quit Neovim", ":qa<CR>"),
 }
-
-
 
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
