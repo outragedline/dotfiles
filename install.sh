c2a54eeca1b6c96127e6a4648c627321bd25d83d
outragedline <outragedline@proton.me>
2023-05-05 20:42:42 -0300

    Rofi: completely new rofi configuration

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Hyprland basic packages
-yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty
+yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git
 
 # Neovim
 yay -S neovim nodejs-lts-hydrogen python3 python-pip ripgrep fd lua gcc npm
