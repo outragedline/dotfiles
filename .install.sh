31c10f98d436c9c97d30ea393383ad6925f601e5
outragedline <outragedline@proton.me>
2023-05-07 23:03:02 -0300

    Install script: add pamixer

--- .install.sh
+++ .install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Hyprland basic packages
-yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git ttf-jetbrains-mono-nerd pipewire wireplumber wl-clipboard
+yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-git hyprpaper-git rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git ttf-jetbrains-mono-nerd pipewire wireplumber wl-clipboard pamixer
 
 # Theme
 yay -S catppuccin-gtk-theme-mocha
