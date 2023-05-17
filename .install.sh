60af1cbf5f8be1762fcdb2cf25c03184197c5433
outragedline <outragedline@proton.me>
2023-05-16 22:05:50 -0300

    Install script: add wget and npm neovim

--- .install.sh
+++ .install.sh
@@ -6,10 +6,10 @@ yay -S hyprland xdg-desktop-portal-hyprland-git grimblast-git waybar-hyprland-gi
 yay -S catppuccin-gtk-theme-mocha
 
 # Neovim
-yay -S neovim nodejs-lts-hydrogen python3 python-pip ripgrep fd lua gcc npm unzip tidy
+yay -S neovim nodejs-lts-hydrogen python3 python-pip ripgrep fd lua gcc npm unzip tidy wget
 
 pip install wheel pynvim 
-
+sudo npm i -g neovim
 # Other important applications
 yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger udisks2 telegram-desktop ungoogled-chromium-bin chromium-extension-web-store mpv viewnior htop thunderbird zsh lazygit stow github-cli
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh)
