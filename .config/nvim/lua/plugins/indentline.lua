31c7aa562012afa3460edcfcff5a396216416cca
outragedline <outragedline@proton.me>
2023-12-21 16:25:29 -0300

    Nvim: dont show underline on indent scope

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -21,4 +21,4 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-require("ibl").setup { indent = { highlight = highlight } }
+require("ibl").setup { indent = { highlight = highlight }, scope = {show_start=false, show_end = false} }
