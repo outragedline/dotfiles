c4138a9acdcb1dbbbe69fdb6ed4cab7d85c724d4
outragedline <outragedline@proton.me>
2024-01-17 17:25:01 -0300

    Hyprland: change wallpaper tool from hyprpaper to waypaper with swww and add some borders to indicate active window
    Hyprpaper was eating so much ram, around 150MB, while swww consumes around 15MB

--- .install.sh
+++ .install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Hyprland basic packages
-yay -S hyprland xdg-desktop-portal-hyprland grimblast-git waybar-hyprland-git hyprpaper rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git ttf-jetbrains-mono-nerd pipewire pipewire-pulse wireplumber wl-clipboard pamixer dunst libnotify blueberry ripdrag brightnessctl hyprland-workspaces gammastep swayidle watershot
+# Waypaper is a gui tool to define wallpapers and needs a backend, you can use swaybg if you dont have time to compile swww, but if your cpu isnt very old it doesnt lasts reaaally long
+yay -S hyprland xdg-desktop-portal-hyprland grimblast-git waybar  rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git  pipewire pipewire-pulse wireplumber wl-clipboard pamixer dunst libnotify blueberry  brightnessctl hyprland-workspaces swayidle waypaper-git
+
+# Hyprland extra packages
+yay -S watershot ttf-jetbrains-mono-nerd ripdrag gammastep  swww
 
 # Theme
 yay -S catppuccin-gtk-theme-mocha
@@ -10,5 +14,5 @@ yay -S neovim nodejs-lts-hydrogen python3 python-pip ripgrep fd lua gcc npm unzi
 sudo npm i -g neovim
 
 # Other important applications
-yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger ueberzugpp udisks2 telegram-desktop librewolf-bin uget uget uget-integrator uget-integrator-libreworlf mpv viewnior htop thunderbird zsh lazygit stow github-cli blender gimp discord
+yay -S zathura zathura-cb zathura-pdf-poppler qbittorrent ranger ueberzugpp  telegram-desktop librewolf-bin uget  mpv viewnior btop thunderbird zsh lazygit stow github-cli blender gimp discord
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh)
