886bbe9ad383bd7f443090c08d6a57173cca07b2
outragedline <outragedline@proton.me>
2024-02-03 16:57:19 -0300

    Update installation script

--- .install.sh
+++ .install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Hyprland basic packages
 # Waypaper is a gui tool to define wallpapers and needs a backend, you can use swaybg if you dont have time to compile swww, but if your cpu isnt very old it doesnt lasts reaaally long
-yay -S hyprland xdg-desktop-portal-hyprland grimblast-git waybar  rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker-git  pipewire pipewire-pulse wireplumber wl-clipboard pamixer dunst libnotify blueberry  brightnessctl hyprland-workspaces swayidle waypaper-git
+yay -S hyprland xdg-desktop-portal-hyprland grimblast-git waybar rofi-lbonn-wayland-git pavucontrol nwg-look-bin alacritty grim slurp hyprpicker  pipewire pipewire-pulse wireplumber wl-clipboard pamixer dunst libnotify blueberry  brightnessctl hyprland-workspaces swayidle waypaper-git
 
 # Hyprland extra packages
-yay -S watershot ttf-jetbrains-mono-nerd ripdrag gammastep  swaybg
+yay -S watershot ttf-jetbrains-mono-nerd  gammastep  swaybg
 
 # Theme
 yay -S catppuccin-gtk-theme-mocha
