8de863ab2608e07201159d706e862f1079b9357a
outragedline <outragedline@proton.me>
2024-06-02 21:47:50 -0300

    Nvim: add necessary rocks to run rest.nvim
    They should already be here to begin with

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -186,6 +186,9 @@ plugins = {
 		"vhyrro/luarocks.nvim",
 		priority = 1000,
 		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
 	},
 	{
 		"NTBBloodbath/rest.nvim",
