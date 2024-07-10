9733c17a07c8559c872de0517effb87e6b2196ff
outragedline <outragedline@proton.me>
2024-07-10 15:55:56 -0300

    Nvim: lazy sync and format some code

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -2,17 +2,17 @@ local set = vim.opt --short vim.opt in set variable
 
 --  user options {
 set.cursorline = true
-set.number = true -- enable line number
+set.number = true         -- enable line number
 set.relativenumber = true -- enable line number
-set.confirm = true -- confirm on exit
-set.smartindent = true -- autoindent new lines
+set.confirm = true        -- confirm on exit
+set.smartindent = true    -- autoindent new lines
 set.ignorecase = false
-set.scrolloff = 8 -- at  least 8 lines will appear  on the top and bottom of the screen
+set.scrolloff = 8         -- at  least 8 lines will appear  on the top and bottom of the screen
 set.sidescrolloff = 8
 set.updatetime = 100
 set.splitright = true -- new vertical splits are created at right
 set.splitbelow = true -- new horizontal splits are created bellow
-set.numberwidth = 3 -- set 3 characters to number column
+set.numberwidth = 3   -- set 3 characters to number column
 set.cmdheight = 2
 set.cc = ""
 set.showmode = false
@@ -21,8 +21,8 @@ set.showmode = false
 
 -- system options {
 set.signcolumn = "yes"
-set.autoread = true -- autoread files if it was changed
-set.hidden = true -- enable the option of hide a buffer without save the file
+set.autoread = true           -- autoread files if it was changed
+set.hidden = true             -- enable the option of hide a buffer without save the file
 set.clipboard = "unnamedplus" -- enable clipboard support
 set.termguicolors = true
 set.undofile = true
@@ -43,7 +43,7 @@ set.filetype = "indent"
 
 -- fold {
 set.foldmethod = "indent" -- create folds by indent level
-set.foldenable = false -- dont close folds whe open file
+set.foldenable = false    -- dont close folds whe open file
 --}
 
 --other {
