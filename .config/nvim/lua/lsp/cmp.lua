5d2f328ec306273f34b4c93699f1ed9b81d4bed1
outragedline <outragedline@proton.me>
2024-07-10 12:53:33 -0300

    Nvim: switch from C-tab to S-tab on completion navigation

--- .config/nvim/lua/lsp/cmp.lua
+++ .config/nvim/lua/lsp/cmp.lua
@@ -41,7 +41,7 @@ cmp.setup({
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<C-Tab>"] = cmp.mapping(function(fallback)
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expandable() then
