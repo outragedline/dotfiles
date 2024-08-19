3711b4ef16de0b7c491fec2d641e9c88971aeae5
outragedline <outragedline@proton.me>
2024-08-19 01:23:49 -0300

    refactor(nvim/kulala): Remove config that is already the default

--- dot_config/nvim/lua/plugins/kulala.lua
+++ dot_config/nvim/lua/plugins/kulala.lua
@@ -1,19 +1,6 @@
 local icons = require("icons")
 require("kulala").setup({
-	-- default_view, body or headers
 	default_view = "body",
-	-- dev, test, prod, can be anything
-	-- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
-	default_env = "dev",
-	-- enable/disable debug mode
-	debug = false,
-	-- default formatters for different content types
-	formatters = {
-		json = { "jq", "." },
-		xml = { "xmllint", "--format", "-" },
-		html = { "xmllint", "--format", "--html", "-" },
-	},
-	-- default icons
 	icons = {
 		inlay = {
 			loading = icons.ui.time,
@@ -21,7 +8,4 @@ require("kulala").setup({
 			error = icons.diagnostics.Error,
 		},
 	},
-	-- additional cURL options
-	-- e.g. { "--insecure", "-A", "Mozilla/5.0" }
-	additional_curl_options = {},
 })
