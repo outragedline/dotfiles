a0e0c0f987be60e517386253ac65fa745d625756
outragedline <outragedline@proton.me>
2024-09-21 02:57:48 -0300

    refactor(nvim): move the plugins table to the beginning of the file

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,15 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
 plugins = {
 	"nvim-lua/plenary.nvim",
 	"kyazdani42/nvim-web-devicons",
@@ -109,4 +97,16 @@ plugins = {
 		build = "make install_jsregexp",
 	},
 }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(plugins, opts)
