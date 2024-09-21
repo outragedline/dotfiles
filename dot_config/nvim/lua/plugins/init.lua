1a056e875a52c94161bcae0028366f3d1bca8e3e
outragedline <outragedline@proton.me>
2024-09-21 01:42:04 -0300

    refactor(nvim): bufferline

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -18,21 +18,7 @@ plugins = {
 
 	require("plugins.indentline"),
 	require("plugins.lualine"),
-	{
-		"akinsho/bufferline.nvim",
-		lazy = false,
-		keys = {
-			{ "<Tab>", "<cmd>BufferLineCycleNext<CR>" },
-			{ "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>" },
-			{ "<leader>bp", "<cmd>BufferLinePick<CR>" },
-			{ "<leader>bpc", "<cmd>BufferLinePickClose<CR>" },
-			{ "<C-m><C-n>", "<cmd> BufferLineMoveNext<CR>" },
-			{ "<C-m><C-p>", "<cmd> BufferLineMovePrev<CR>" },
-		},
-		config = function()
-			require("bufferline").setup()
-		end,
-	},
+	require("plugins.bufferline"),
 	{
 		"tiagovla/scope.nvim",
 		config = function()
