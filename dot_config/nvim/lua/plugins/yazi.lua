54f3f064f920a05f2dec5f8aed0e4a5fd4ffc436
outragedline <outragedline@proton.me>
2024-12-16 18:40:16 -0300

    fix(nvim): change conflicting yazi keybinding

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -17,7 +17,7 @@ return {
 		{
 			-- NOTE: this requires a version of yazi that includes
 			-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-			"<c-up>",
+			"<c-r><c-a>",
 			"<cmd>Yazi toggle<cr>",
 			desc = "Resume the last yazi session",
 		},
