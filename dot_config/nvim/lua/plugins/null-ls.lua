4dec8d45aabf5abbbd06b9ea3313dbfa3ba62d40
outragedline <outragedline@proton.me>
2025-02-06 01:21:11 -0300

    fix(nvim): Fix null ls sources not working

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -5,7 +5,7 @@ return {
 		"williamboman/mason.nvim",
 		"nvimtools/none-ls.nvim",
 	},
-	setup = function()
+	config = function()
 		local null_ls = require("null-ls")
 		local formatting = null_ls.builtins.formatting
 		local diagnostics = null_ls.builtins.diagnostics
